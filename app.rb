5573dd6e2c55ccc17517e4a8a4ecb4cbfedce05d
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-23 16:54:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    num1 = params[:number1]
-    num2 = params[:number2]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
   end
 
 end
