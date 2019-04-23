bc114dc8891c05b98fb441e02b81871a62065405
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-23 16:51:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    params[:number].times do params[:phrase] end
+    params[:phrase] * params[:number]
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
